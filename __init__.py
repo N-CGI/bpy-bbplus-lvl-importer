4c59254e14cd4164a4cddffb5f7ec1ecc269cb20
N-CGI <nathanmcjohns@gmail.com>
2026-05-23 10:15:08 +1000

    Loading Cell behaviour

--- __init__.py
+++ __init__.py
@@ -19,9 +19,11 @@ bl_info = {
     "category": "Import-Export",
 }
 
-import bpy
+import bpy# type: ignore
 import os
+import imbuf# type: ignore
 from .lib import global_constructor as Pbpl
+from .lib import level_constructor as Level
 from bpy_extras.io_utils import ImportHelper # pyright: ignore[reportMissingModuleSource]
 from bpy.props import StringProperty # pyright: ignore[reportMissingModuleSource]
 from bpy.types import Operator # pyright: ignore[reportMissingModuleSource]
@@ -29,7 +31,6 @@ from bpy.types import Operator # pyright: ignore[reportMissingModuleSource]
 def imbuf_to_bpy_image(imbuf_img, image_name="ConvertedImage"):
     # 1. Define a temporary path to hold the file
     temp_path = os.path.join(bpy.app.tempdir, "temp_transfer.png")
-    
     # 2. Use Blender's imbuf module to write out the data to disk
     imbuf.write(imbuf_img, filepath=temp_path)
     
@@ -50,12 +51,38 @@ class IMPORT_OT_bbpl_format(Operator, ImportHelper):
     bl_idname = "import_scene.pbpl"
     bl_label = "Import BB+ Level (.pbpl)"
     bl_options = {'PRESET', 'UNDO'}
-
+    filter_glob: StringProperty(
+        default="*.pbpl;",
+        options={'HIDDEN'},
+    ) # type: ignore
+    #i guess fake-bpy module doesnt recognise String Property yet
     def execute(self, context):
         # Call the parser function
         load_custom_file(self.filepath)
         return {'FINISHED'}
 
+#Location Rotation pairs
+pi : float = 3.14159265
+wallrotations=[
+    (0,0.5,0),(0,-pi/2,pi/2),
+    (0.5,0,0),(0,-pi/2,0),
+    (0,-0.5,0),(0,-pi/2,-pi/2),
+    (-0.5,0,0),(0,-pi/2,pi),
+]
+def loadCells(level : Level.Level):
+    cells=level.cells
+    for i in range(level.width*level.height):
+        x=i//level.width
+        y=i%level.height
+        print(level.cells[i].walls)
+        loadCell(level.cells[i],[x,y,0])
+        
+def loadCell(cell,pos):
+    for i in range(4):
+        if (cell.walls)&(1<<i):
+            print("the wall nybble: ",cell.walls," binary contains the ",2*i+2,"'s digit!")
+            bpy.ops.mesh.primitive_plane_add(size=1, align='WORLD', location=[wallrotations[i*2][0]+pos[0],wallrotations[i*2][1]+pos[1],wallrotations[i*2][2]+pos[2]], rotation=wallrotations[i*2+1])
+            bpy.ops.object.origin_set(type='ORIGIN_CURSOR', center='MEDIAN')
 def load_custom_file(filepath):
     with open(filepath,"rb") as f:
         map=Pbpl.LevelStudioMap(f)
@@ -67,6 +94,7 @@ def load_custom_file(filepath):
                 file=open(filepath+entry.data,"rb")
                 freequeue.append(file)
                 entry.data=file
+        loadCells(map.level)
         for entry in freequeue:
             entry.close()
 
